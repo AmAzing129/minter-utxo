6ba0b95ba293bf7b6da15e62ff102abb144b78a8
AmAzing129 <web2crypto@proton.me>
2024-09-13 15:51:01 +0800

    init

---
+++ src/app/api/route.ts
@@ -0,0 +1,34 @@
+ 
+export async function GET() {
+  const res = await fetch("https://explorer.unisat.io/fractal-mainnet/api/address/summary?address=bc1pqw9ncs4sna0ndh85ux5dhh9swueyjql23t4em8j0smywkqsngfmsn7gmua", {
+    "headers": {
+      "accept": "application/json, text/plain, */*",
+      "accept-language": "zh-CN,zh;q=0.9",
+      "cache-control": "no-cache",
+      "cf-token": "vcnf7le1ec151xdxushjshh5b",
+      "fetch-mode": "no-cors",
+      "fetch-site": "same-origin",
+      "pragma": "no-cache",
+      "priority": "u=1, i",
+      "sec-ch-ua": "\"Chromium\";v=\"130\", \"Google Chrome\";v=\"130\", \"Not?A_Brand\";v=\"99\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"macOS\"",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "x-appid": "1adcd7969603261753f1812c9461cd36",
+      "x-front-version": "126",
+      "x-sign": "7b2dd631389ee1ff4b36d0f934c18ecc",
+      "x-ts": "1726209251"
+    },
+    "referrer": "https://explorer.unisat.io/fractal-mainnet/address/bc1pqw9ncs4sna0ndh85ux5dhh9swueyjql23t4em8j0smywkqsngfmsn7gmua",
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": null,
+    "method": "GET",
+    "mode": "cors",
+    "credentials": "include"
+  })
+  const data = await res.json()
+ 
+  return Response.json({ data: data.data })
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,95 @@
+// @ts-nocheck
+"use client";
+import { useEffect, useState } from "react";
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+
+function calculateMinuteDifference(startDate: Date, endDate: Date): number {
+  // Step 1: Get time values in milliseconds
+  const startTimeMs = startDate?.getTime();
+  const endTimeMs = endDate?.getTime();
+
+  // Step 2: Calculate the difference in milliseconds
+  const differenceMs = endTimeMs - startTimeMs;
+
+  // Step 3: Convert the difference from milliseconds to minutes
+  const differenceMinutes = differenceMs / (1000 * 60);
+
+  return differenceMinutes;
+}
+
+export default function Home() {
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  )
+
+  const [utxo, setUtxo] = useState<[{ time: Date, utxoCount: number}]>([{ time: new Date(), utxoCount: 0 }]);
+
+  const chartData = {
+    labels: utxo.map(d => d.time.toLocaleTimeString()),
+    datasets: [
+      {
+        label: 'UTXO Count',
+        data: utxo.map(d => d.utxoCount),
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+      }
+    ]
+  }
+
+  useEffect(() => {
+    
+    const id = setInterval(() => {
+      fetch('/api')
+        .then((res) => res.json())
+        .then((data) => {
+          setUtxo(u => [...u, { time: new Date(), utxoCount: data.data.utxo_count }]);
+        })
+    }, 5000);
+
+    return () => clearInterval(id);
+
+  }, [])
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'utxoCount'
+      }
+    },
+    scales: {
+      y: {
+        min: 100000,
+        max: 170000,
+      }
+    }
+  }
+
+  const v  = (utxo[2]?.utxoCount - utxo[utxo.length - 1].utxoCount) / calculateMinuteDifference(utxo[2]?.time, utxo[utxo.length - 1].time);
+
+  return (<>
+    <div>预计{(utxo[utxo.length - 1]?.utxoCount / v / 60).toFixed(2)}小时后结束</div>
+    <div style={{ width: 1000, height: 1000 }}><Line options={options} data={chartData} /></div>
+  </>);
+}
